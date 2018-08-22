126dca8914387397ccd5dedb45714a69a48d9371
Stephan Schrijver <stephanschrijver@gmail.com>
2018-08-22 09:07:51 +0200

    Fix lint errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import {EffectsModule} from "@ngrx/effects";
-import {StoreModule} from "@ngrx/store";
-import {NgrxActionsModule} from "ngrx-actions/dist";
-import {StoreRouterConnectingModule} from "@ngrx/router-store";
-import {StoreDevtoolsModule} from "@ngrx/store-devtools";
-import {environment} from "../environments/environment";
+import {AppComponent} from './app.component';
+import {EffectsModule} from '@ngrx/effects';
+import {StoreModule} from '@ngrx/store';
+import {NgrxActionsModule} from 'ngrx-actions/dist';
+import {StoreRouterConnectingModule} from '@ngrx/router-store';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {environment} from '../environments/environment';
+
 
 @NgModule({
   declarations: [
